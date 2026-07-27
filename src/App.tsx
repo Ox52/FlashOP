7ca833429e27d7f88cdc365c938633d236f43f2d
Ox52 <harsh04241@gmail.com>
2026-07-27 16:23:19 +0530

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,48 @@
 
+import { useState } from "react";
 import "../styles/globals.css";
 import Navbar from "./components/Navbar";
-
+import Home from "./pages/Home";
+import Quiz from "./pages/Quiz";
+import { Routes, Route } from "react-router-dom";
+import type { Flashcard } from "./types/quiz";
 
 
 
 export function App() {
+
+  const [quizStarted , setQuizStarted] =useState(false)
+  const [questions, setQuestions] = useState<Flashcard[]>([]);
   return (
+    <>
+    <Navbar/>
+
+<main className=" min-h-screen bg-background">
+
+ {!quizStarted? (
+
+  <Home 
+  
+  setQuizStarted ={setQuizStarted}
+  setQuestions={setQuestions}
+  />
+ ) :(
+
+  <Quiz questions={questions} />
+ )}
 
 
-       <div className=" min-h-screen bg-background">
 
-<Navbar/>
-</div>
+
+
+</main>
+    
+    
+    </>
+    
+
+
+  
     
   
    
